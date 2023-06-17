d7dbdb81c5b91ebc5bbdb5d44a7d2bf3d43342b7
dinhquan2007 <dinhquan2007@gmail.com>
2023-06-17 11:55:13 +0700

    main

--- case_study_1/case_study_task_4.sql
+++ case_study_1/case_study_task_4.sql
@@ -24,7 +24,9 @@ WHERE
 
 
 SELECT 
-    khach_hang.ma_khach_hang, khach_hang.ho_ten, COUNT(*) AS so_lan_dat_phong
+    khach_hang.ma_khach_hang,
+    khach_hang.ho_ten,
+    COUNT(*) AS so_lan_dat_phong
 FROM
     khach_hang
         JOIN
@@ -35,4 +37,28 @@ WHERE
     loai_khach_hang.ten_loai_khach = 'Diamond'
 GROUP BY hop_dong.ma_khach_hang
 ORDER BY so_lan_dat_phong;
+-- 5
+SELECT 
+    khach_hang.ma_khach_hang,
+    khach_hang.ho_ten,
+    loai_khach_hang.ten_loai_khach,
+    hop_dong.ma_hop_dong,
+    dich_vu.ten_dich_vu,
+    hop_dong.ngay_lam_hop_dong,
+    hop_dong.ngay_ket_thuc,
+    (dich_vu.chi_phi_thue + (dich_vu_di_kem.gia * hop_dong_chi_tiet.so_luong)) AS tong_tien
+FROM
+    khach_hang
+        LEFT JOIN
+    loai_khach_hang ON khach_hang.ma_loai_khach = loai_khach_hang.ma_loai_khach
+        LEFT JOIN
+    hop_dong ON khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem;
+
+
 
